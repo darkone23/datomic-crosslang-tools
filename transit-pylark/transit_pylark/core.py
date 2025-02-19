716a5fe5cb118aa6073cc1fb0cb6a0a03bd98a26
orpheus <orpheus@computerdream.club>
2025-02-18 22:02:57 -0800

    Basics of pluggable extensions
    - Implement 'quote' tag extension

--- transit-pylark/transit_pylark/core.py
+++ transit-pylark/transit_pylark/core.py
@@ -1,10 +1,30 @@
 from pathlib import Path
+import time
 import json
 
 from rich.pretty import pprint
-from lark import Lark, Transformer, Discard
+from lark import Lark, Transformer, Discard, Tree
 
 from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class transit_tag:
+    tag: str
+
+    @staticmethod
+    def tagged_value(tag: str, value):
+        return (transit_tag(tag), value)
+
+    def __len__(self):
+        return len(self.tag)
+
+    def __str__(self):
+        return f":{self.tag}"
+
+    @staticmethod
+    def tag_key(name):
+        return f"tag:{name}"
 
 
 @dataclass(frozen=True)
@@ -23,6 +43,17 @@ class mapkey:
     k: str
 
     def __len__(self):
+        return len(self.k)
+
+    def __str__(self):
+        return f"{self.k}"
+
+
+@dataclass(frozen=True)
+class quoted:
+    v: str
+
+    def __len__(self):
         return len(self.v)
 
     def __str__(self):
@@ -33,23 +64,27 @@ class mapkey:
 # should support user defined extensions in the transformers
 
 TRANSIT_STRING_GRAMMAR = """
-value: nil
-     | tag
-     | bool_t
-     | bool_f
-     | base64
-     | keyword
-     | symbol
+value: "~" instruction [ chars ]
 
-nil:  "~_"
-tag:  "~#" chars
-bool_t:  "~?t"
-bool_f:  "~?f"
-base64:  "~b" chars
-keyword:  "~:" chars
-symbol:  "~$" chars
+instruction: nil
+         | escape_tilde
+         | escape_hat
+         | bool
+         | base64
+         | keyword
+         | symbol
+         | tag
+
+nil: "_"
+escape_tilde: "~"
+escape_hat: "^"
+bool: "?"
+base64: "b"
+keyword: ":"
+symbol: "$"
+tag: "#"
+
 chars: /.+/
-raw_str: /.+/
 """
 
 TRANSIT_GRAMMAR = """
@@ -77,23 +112,94 @@ transit_num : SIGNED_NUMBER
 """
 
 
+class TransitSerde:
+
+    def deserialize(self, encoded: str):
+        pass
+
+
+class KeywordSerde:
+
+    def deserialize(self, encoded: str):
+        return keyword(encoded)
+
+
+class TagSerde:
+
+    def deserialize(self, encoded: str):
+        return transit_tag(encoded)
+
+
+class QuoteTagSerde:
+
+    def deserialize(self, encoded: str):
+        return quoted(encoded)
+
+
+class TransitTypeResolver:
+
+    def __init__(self):
+        self.mapping = {}
+
+    def add_serde(self, name: str, serde: TransitSerde):
+        self.mapping[name] = serde
+
+    def resolve(self, name: str, value: str | None = None):
+        if value is not None:
+            value = str(value)
+        if name in self.mapping:
+            # print("looking up:", name, value)
+            return self.mapping[name].deserialize(value)
+        else:
+            # print("tagged", name, value)
+            return transit_tag.tagged_value(name, value)
+
+    @staticmethod
+    def default():
+        resolver = TransitTypeResolver()
+        # these names matching the lark grammar names
+        resolver.add_serde("keyword", KeywordSerde())
+        resolver.add_serde("tag", TagSerde())
+        resolver.add_serde("tag:'", QuoteTagSerde())
+        return resolver
+
+
 class TransitScalarTransformer(Transformer):
 
+    def __init__(self, resolver: TransitTypeResolver):
+        self.resolver = resolver
+
     def value(self, args):
-        (v,) = args
-        return v
+        arg_size = len(args)
+        if arg_size == 1:
+            (v,) = args
+            # must be: escape str or nil
+            # print("one args???")
+            return self.resolver.resolve(k)
+            return v
+        elif arg_size == 2:
+            (k, v) = args
+            # print("two args???", k, v)
+            return self.resolver.resolve(k, v)
+        else:
+            raise ValueError(f"Unexpected transit str parser len... {len(args)}")
 
-    def keyword(self, s):
-        (s,) = s
-        return keyword(f"{s}")
+    def chars(self, c):
+        (t,) = c
+        # (c,) = t.children
+        return t
 
-    def str(self, s):
+    def instruction(self, s):
         (s,) = s
-        return s
+        return s.data
 
-    def chars(self, s):
-        (s,) = s
-        return s
+    # def str(self, s):
+    #     (s,) = s
+    #     return s
+
+    # def chars(self, s):
+    #     (s,) = s
+    #     return s
 
 
 class TransitCacheControl:
@@ -157,6 +263,9 @@ class TransitCacheControl:
 # transformer can be responsible for cache reading layer
 class TransitJsonTransformer(Transformer):
 
+    CACHE_MAP_TOKEN = "^ "
+    CACHE_TOKEN = "CACHECODE:^"
+
     def __cache_analysis(self, value):
         v_type = type(value)
         res = dict(should_cache=False, v_type=v_type, cache_token=None)
@@ -164,10 +273,10 @@ class TransitJsonTransformer(Transformer):
             return res
         if v_type is str:
             v_size = len(value)
-            if value == "CACHE_MAP_TOKEN":
+            if value == self.CACHE_MAP_TOKEN:
                 res["v_type"] = "map-token"
                 return res
-            elif value.startswith("CACHECODE:^"):
+            elif value.startswith(self.CACHE_TOKEN):
                 res["cache_token"] = value[10:]
             # else:
             # res["should_cache"] = v_size > 3
@@ -206,12 +315,15 @@ class TransitJsonTransformer(Transformer):
             return popped
         return s
 
-    def __init__(self):
+    def __init__(self, resolver: TransitTypeResolver | None = None):
+        if resolver is None:
+            resolver = TransitTypeResolver.default()
+        self.resolver = resolver
         self.transit = Lark(
             TRANSIT_STRING_GRAMMAR,
             start="value",
             parser="lalr",
-            transformer=TransitScalarTransformer(),
+            transformer=TransitScalarTransformer(resolver=resolver),
         )
         self.__cache = {}
         self.__cache_idx = 0
@@ -220,21 +332,23 @@ class TransitJsonTransformer(Transformer):
         (n,) = args
         return float(n)
 
-    def dict(self, args):
-        res = {}
-        for pair in args:
-            (k, v) = pair
-            res[mapkey(k)] = v
-        return res
-
     def pair(self, args):
         (k, v) = args
         return (k, v)
 
+    def false(self, args):
+        return False
+
+    def true(self, args):
+        return True
+
+    def null(self, args):
+        return None
+
     # list = list
     # pair = tuple
 
-    def list(self, args):
+    def __list(self, args):
         if len(args) == 0:
             # print("empty list")
             return []
@@ -245,7 +359,7 @@ class TransitJsonTransformer(Transformer):
         else:
             head = args[0]
             # print("list", head, args)
-            if head == "CACHE_MAP_TOKEN":
+            if head == self.CACHE_MAP_TOKEN:
                 res = {}
                 for n in range(int(len(args) / 2)):
                     idx = n * 2 + 1
@@ -256,6 +370,41 @@ class TransitJsonTransformer(Transformer):
             else:
                 return args
 
+    def __dict(self, args):
+        res = {}
+        for pair in args:
+            (k, v) = pair
+            res[mapkey(k)] = v
+        return res
+
+    def __transit_dict(self, xs):
+        assert len(xs) == 1, "I don't know what to do with more than 2 entries"
+        (k, v) = next(iter(xs.items()))
+        tag_key = transit_tag.tag_key(k.k.tag)
+        return self.resolver.resolve(tag_key, v)
+
+    def dict(self, args):
+        xs = self.__dict(args)
+        # print(xs)
+        if len(xs) and type(next(iter(xs.keys())).k) is transit_tag:
+            xs = self.__transit_dict(xs)
+        return xs
+
+    def __transit_list(self, xs):
+        assert len(xs) == 2, "I don't know what to do with more than 2 entries"
+        (k, v) = xs
+        tag_key = transit_tag.tag_key(k.tag)
+        return self.resolver.resolve(tag_key, v)
+
+    def list(self, args):
+        xs = self.__list(args)
+        if type(xs) is dict:
+            if len(xs) and type(next(iter(xs.keys())).k) is transit_tag:
+                xs = self.__transit_dict(xs)
+        elif len(xs) and type(xs[0]) is transit_tag:
+            xs = self.__transit_list(xs)
+        return xs
+
     def transit_str(self, args):
         # self.parser.parse()
         (s,) = args
@@ -263,52 +412,99 @@ class TransitJsonTransformer(Transformer):
         # print("you casked me to handle trnsit str?", encoded_str)
         transit_part = encoded_str[1:-1]  # not needing "" parts of str
         if transit_part.startswith("~"):
-            try:
-                result = self.transit.parse(transit_part)
-            except Exception as e:
-                print("Oh no an exception!", e)
-                result = transit_part
+            # special transit escaped string
+            result = self.transit.parse(transit_part)
+            # try:
+            # except Exception as e:
+            #     print("Exception parsing transit encoded str node!", e)
+            #     # print(e)
+            #     result = transit_part
         elif transit_part.startswith("^"):
+            # special cache instruction
             remainder = transit_part[1:]
             if remainder == " ":
-                result = f"CACHE_MAP_TOKEN"
+                result = self.CACHE_MAP_TOKEN
             else:
-                result = f"CACHECODE:^{remainder}"
+                result = self.CACHE_TOKEN + remainder
         else:
             result = transit_part
         return result
 
 
+class TransitExampleRunner:
+
+    def __init__(self, parser: Lark, source: Path):
+        self.name = source.name
+        self.transit_txt = (
+            source.read_text()
+        )  # """["~:a","~:ab","~:abc","~:abcd","~:abcde","~:a1","~:b2","~:c3","~:a_b"]"""
+        self.parser: Lark = parser
+
+    def run_parse(self):
+
+        payload_size = len(self.transit_txt)
+        # print(f"Parsing {self.name} json document of len", payload_size)
+
+        start = time.monotonic()
+
+        tree = self.parser.parse(self.transit_txt)
+        # print(tree.pretty())
+
+        parse_time = time.monotonic() - start
+        # print(f"Transforming {self.name} document after n seconds: ", parse_time)
+
+        # pprint(tree)
+        # result = transformer.transform(tree)
+        # print(result.pretty())
+
+        # json.loads(transit_txt)
+
+        # print(f"Total {self.name} took n seconds:", time.monotonic() - start)
+
+        return dict(
+            name=self.name,
+            size=payload_size,
+            parse_time=parse_time,
+            tree=tree,
+        )
+
+
 def main():
-    import time
 
     # from rich.pretty import pprint
-
-    transit_txt = Path(
-        "./test_data/example.json"  # cached version of data
-    ).read_text()  # """["~:a","~:ab","~:abc","~:abcd","~:abcde","~:a1","~:b2","~:c3","~:a_b"]"""
-    # transit_txt = Path("./test_data/example.verbose.json").read_text() # """["~:a","~:ab","~:abc","~:abcd","~:abcde","~:a1","~:b2","~:c3","~:a_b"]"""
-    transformer = TransitJsonTransformer()
     parser = Lark(
-        TRANSIT_GRAMMAR, start="value", parser="lalr", transformer=transformer
+        TRANSIT_GRAMMAR,
+        start="value",
+        parser="lalr",
+        transformer=TransitJsonTransformer(),
     )
 
-    print("Parsing json document of len", len(transit_txt))
+    transit_cache_example = Path(
+        # transit cache encoded data
+        "./test_data/example.json"
+        # "./test_data/simple/nil.json"
+    )
 
-    start = time.monotonic()
+    transit_verbose_example = Path(
+        # transit verbose encoded data
+        "./test_data/example.verbose.json"
+        # "./test_data/simple/nil.verbose.json"
+    )
 
-    tree = parser.parse(transit_txt)
-    # print(tree.pretty())
+    results = []
+    for source in [
+        transit_cache_example,
+        transit_verbose_example,
+    ]:
+        tool = TransitExampleRunner(parser, source)
+        results.append(tool.run_parse())
 
-    print("Transforming document after n seconds: ", time.monotonic() - start)
+    assert results[0]["tree"] == results[1]["tree"], "parsed trees are identical!"
 
-    pprint(tree)
-    # result = transformer.transform(tree)
-    # print(result.pretty())
-
-    # json.loads(transit_txt)
-
-    print("Took n seconds:", time.monotonic() - start)
+    for run in results:
+        # pprint(run["tree"])
+        del run["tree"]  # just print out stats for now
+        pprint(run)
 
 
 if __name__ == "__main__":
